c6a6990759ef83c78acb8e7773fb0c1db2b143e1
shahadat10033 <shahadat10033@gmail.com>
2023-05-19 18:30:23 +0600

    single toy information collected from momgodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
@@ -26,11 +26,18 @@ async function run() {
     const dbCollection = client.db("ToysDB").collection("toys");
 
     app.get("/toys", async (req, res) => {
-      const limit = 10;
+      const limit = 20;
       const result = await dbCollection.find().limit(limit).toArray();
 
-      // Send the limited data as a response
-      res.json(result);
+      res.send(result);
+    });
+
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await dbCollection.findOne(query);
+      res.send(result);
+      console.log(result);
     });
 
     app.post("/toys", async (req, res) => {
